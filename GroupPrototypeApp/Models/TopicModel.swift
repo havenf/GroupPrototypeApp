ee27a5aa78bb196e042ab7771db51b713ac60eee
Haven Fricke <havenfricke@u.boisestae.edu>
2025-03-06 22:54:00 -0700

    Model changes

--- GroupPrototypeApp/Models/TopicModel.swift
+++ GroupPrototypeApp/Models/TopicModel.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct TopicModel {
     let title: String
-    let imageName: String
     let destination: AnyView
 }
 
@@ -17,32 +16,26 @@ struct Topic {
     static let topics: [TopicModel] = [
         TopicModel(
                 title: "Accessibility and SwiftUI",
-                imageName: "accessibilitySwiftUI",
                 destination: AnyView(SecondView())
             ),
         TopicModel(
                 title: "Accessible Spatial Experiences",
-                imageName: "spatialExperiences",
                 destination: AnyView(ThirdView())
             ),
         TopicModel(
                 title: "Dynamic Type",
-                imageName: "dynamicType",
                 destination: AnyView(FourthView())
             ),
         TopicModel(
                 title: "Assistive Access",
-                imageName: "assistiveAccess",
                 destination: AnyView(FifthView())
             ),
         TopicModel(
                 title: "Extend Speech Synthesis",
-                imageName: "speechSynthesis",
                 destination: AnyView(SixthView())
             ),
         TopicModel(
                 title: "Getting Started with Writing Tools",
-                imageName: "writingTools",
                 destination: AnyView(SeventhView())
             )
         ]
